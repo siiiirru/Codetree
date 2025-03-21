31fe734c5e346bd791c4618b3ec920e44efcc820
Siyun Park <eqeee@naver.com>
2025-03-21 23:27:29 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 250321/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+def print_rect():
+    for _ in range(n):
+        for _ in range(m):
+            print("1",end="")
+        print("")
+
+print_rect()
+# Please write your code here.
