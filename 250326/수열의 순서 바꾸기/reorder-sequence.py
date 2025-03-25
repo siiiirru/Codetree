0eb4d2e9660a873a84b83c365a1ed3491e13a37f
Siyun Park <eqeee@naver.com>
2025-03-26 04:37:36 +0900

    Title: 수열의 순서 바꾸기, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250326/수열의 순서 바꾸기/reorder-sequence.py
+++ 250326/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,35 +1,49 @@
+from collections import deque
 n = int(input())
-sequence = list(map(int, input().split()))
-sorted_sequence= sorted(sequence)
-
-first_min= True
-count= 0
+sequence = deque(list(map(int, input().split())))
+sorted_sequence = deque(sorted(sequence))
+count = 0
+min_val = min(sequence)
+max_val = max(sequence)
+min_first = True
 # Please write your code here.
-for i in range (n):
-    if sequence == sorted_sequence: 
-        break
-
-    min_val=min(sequence)
-    max_val=max(sequence)
-
-    if sequence[0] == min_val:
-        if first_min == True:
-            max_index= sequence.index(max_val)
-            sequence.pop(0)
-            sequence.insert(max_index,min_val)
-            count+=1
-    elif sequence[0] == max_val:
-        sequence.pop(0)
-        sequence.append(max_val)
-        count+=1
-    else: 
-        min_index=sequence.index(min_val)
-        max_index=sequence.index(max_val)
-        for j in range(min(n,max_index+1),n):
-            if sequence[0]>sequence[j] or j==n:
-                val=sequence.pop(0)
-                sequence.insert(j,val)
-                count+=1
+if sequence != sorted_sequence:
+    for i in range (n): 
+        val=sequence.popleft()
+        if val == max_val:
+            sequence.append(val)
+            count += 1
+        elif val == min_val:
+            if min_first == False : 
                 break
+            else : 
+                min_first = False
+            past_tail = sequence[-1]
+            if past_tail == max_val:
+                past_tail = sequence[-2]
+            for j in range(n-3,-1,-1):
+                if past_tail < sequence[j]:
+                    sequence.insert(j+1,val)
+                    count+=1
+                    break
+        else :
+            past_tail = sequence[-1]
+            if past_tail == max_val:
+                past_tail = sequence[-2]
+            for j in range(n-3,-1,-1):
+                if past_tail < sequence[j]:
+                    start = j
+                    new_sequence = [val-x for x in sequence]
+                    new_sequence_positive = list(map(lambda x : 100 if x<0 else x, new_sequence))
+                    new_sequence_positive[0:j]= [100] * (j)
+                    insert_index = new_sequence_positive.index(min(new_sequence_positive))
+                    sequence.insert(insert_index+1,val)
+                    count+=1
+                    break
+                if past_tail < val :
+                    sequence.insert(-1,val)
+                    count+=1
+                    break
+            
 
 print (count)
