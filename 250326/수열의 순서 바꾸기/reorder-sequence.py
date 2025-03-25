f1570122a8e168405224717c77d5d08594cbf78d
Siyun Park <eqeee@naver.com>
2025-03-26 04:50:39 +0900

    Title: 수열의 순서 바꾸기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250326/수열의 순서 바꾸기/reorder-sequence.py
+++ 250326/수열의 순서 바꾸기/reorder-sequence.py
@@ -2,8 +2,9 @@ n = int(input())
 blocks = list(map(int, input().split()))
 count = 0
 for i in range(n-1,-1,-1):
-    if i-1>=0 and blocks[i-1]>blocks[i] :
+    if blocks[i-1]>blocks[i] :
         count = i
+        break
             
 
 print (count)
