146859284c20cb4065a911892c9485205cf66dbc
Siyun Park <eqeee@naver.com>
2025-03-25 23:06:32 +0900

    Title: 움직이는 블록, Time: 273ms, Memory: 19MB, Status: Passed - CodeTree

--- 250325/움직이는 블록/moving-block.py
+++ 250325/움직이는 블록/moving-block.py
@@ -11,8 +11,14 @@ for i in range(n):
     while over_blocks[i]<0 :
         max_value=(max(over_blocks))
         max_index=over_blocks.index(max_value)
-        over_blocks[max_index]+=over_blocks[i]
-        count+=over_blocks[i]*-1
-        over_blocks[i]=0
+        if max_value>(over_blocks[i]*-1):
+            over_blocks[max_index]+=over_blocks[i]
+            count+=over_blocks[i]*-1
+            over_blocks[i]=0
+        else :
+            over_blocks[max_index]=0
+            over_blocks[i]+=max_value
+            count+=max_value
+            
 
 print(count)    
