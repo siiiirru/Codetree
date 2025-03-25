ce43d8abbe88af2aa3b774dc0c45e67819398ab4
Siyun Park <eqeee@naver.com>
2025-03-25 23:13:05 +0900

    Title: 움직이는 블록, Time: 84ms, Memory: 18MB, Status: Passed - CodeTree

--- 250325/움직이는 블록/moving-block.py
+++ 250325/움직이는 블록/moving-block.py
@@ -2,23 +2,11 @@ n = int(input())
 blocks = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-blocks_number=sum(blocks)//n
+avg=sum(blocks)//n
 count=0
 
-over_blocks=[x-blocks_number for x in blocks]
-
 for i in range(n):
-    while over_blocks[i]<0 :
-        max_value=(max(over_blocks))
-        max_index=over_blocks.index(max_value)
-        if max_value>(over_blocks[i]*-1):
-            over_blocks[max_index]+=over_blocks[i]
-            count+=over_blocks[i]*-1
-            over_blocks[i]=0
-        else :
-            over_blocks[max_index]=0
-            over_blocks[i]+=max_value
-            count+=max_value
-            
+    if blocks[i]>avg:
+        count+=blocks[i]-avg
 
-print(count)    
+print(count)
