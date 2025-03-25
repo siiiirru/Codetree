46b211081b101e7b34d9ce9d97b7da3c3ad4952a
Siyun Park <eqeee@naver.com>
2025-03-26 01:15:47 +0900

    Title: 홀수 짝수의 묶음, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250326/홀수 짝수의 묶음/odd-even-bundle.py
+++ 250326/홀수 짝수의 묶음/odd-even-bundle.py
@@ -15,17 +15,14 @@ for i in range(N):
 if 0<=(even - odd)<2:
     result=N
 else:
-    diff=abs(odd-even)
-    # 홀, 짝 중 하나의 갯수가 0이 아닌 경우
-    if diff!=N: 
-        result=min(odd,even)*2
-        if odd>even:
-            if diff%2==0: result+=diff//2
-            else : result+=diff//2+1
-        else : result+=1
-    elif odd==N: 
-        result=N//2
-        if N%2==0:result+=1
-    else : result=1 
-    
+    if even>odd:
+        result=odd*2+1
+    else :
+        result=even*2
+        diff=odd-even
+        if diff%2==0:
+            result+=diff//2+1
+        elif (diff//2)%2==0: result+=diff//2+1
+        else : result+=diff//2
+
 print (result)
