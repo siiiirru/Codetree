5d90f0aa8647c2f6f02ec1ddcf18838bb5f7f70c
Siyun Park <eqeee@naver.com>
2025-03-25 23:34:21 +0900

    Title: 최소 와이파이 수, Time: 5076ms, Memory: 16MB, Status: Time Limit Exceed - CodeTree

--- 250325/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250325/최소 와이파이 수/minimum-number-of-wifi.py
@@ -3,10 +3,15 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 cnt=0
-for i in range(0,n,m*2+1):
+i=0
+while i<n:
+    if arr[i]==0: 
+        i+=1 
+        continue
     for j in range (i,i+m*2):
         if arr[j]==1: 
             cnt+=1
+            i+=m*2+1
             break
 
 print(cnt)
