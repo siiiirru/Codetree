e6df0aa7e06558036a19496a17d08153c21cb398
Siyun Park <eqeee@naver.com>
2025-03-25 23:30:48 +0900

    Title: 최소 와이파이 수, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250325/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250325/최소 와이파이 수/minimum-number-of-wifi.py
@@ -4,8 +4,6 @@ arr = list(map(int, input().split()))
 # Please write your code here.
 cnt=0
 for i in range(0,n,m*2+1):
-    if arr[i]==0:
-        continue
     for j in range (i,i+m*2):
         if arr[j]==1: 
             cnt+=1
