415a3ad967dbf5858c06023362b711410a36b152
Siyun Park <eqeee@naver.com>
2025-04-14 17:11:06 +0900

    Title: 구슬의 이동, Time: 136ms, Memory: 20MB, Status: Wrong Answer - Codetree

--- 250414/구슬의 이동/marble-movement.py
+++ 250414/구슬의 이동/marble-movement.py
@@ -33,7 +33,7 @@ def change_dir(original_d):
 
 # 시간의 흐름. 1초부터 시작
 def move_marbles():
-    for time in range(1,t+1):
+    for time in range(t):
         # 구슬들의 이동
         for i in range (m):
             # 활성화 된 구슬만 이동
@@ -49,8 +49,8 @@ def move_marbles():
                     # 벽에 부딪혔을 때는 방향 바꿔서 이동
                     if dy<0 or dy>=n or dx<0 or dx>=n :
                         d[i]=change_dir(d[i])
-                        dy=r[i]+2*dir[d[i]][0]
-                        dx=c[i]+2*dir[d[i]][1]
+                        dy=r[i]+dir[d[i]][0]
+                        dx=c[i]+dir[d[i]][1]
                     r[i]=dy
                     c[i]=dx
 
@@ -58,27 +58,24 @@ def move_marbles():
                 if (r[i],c[i]) in marbles:
                     marbles[(r[i],c[i])].append(i)
                 else: marbles[(r[i],c[i])]=[i]
-            
+
         # 충돌 시 사라짐
         for value in marbles.values():
             while len(value)>k:
                 # 속도가 작은 것 부터 비활성화
                 vlist = [v[i] for i in value]
                 min_val=min(vlist)
-                min_val_index=[ind for ind,val in enumerate(v) if val==min_val]
+                min_val_index=[ind for ind,val in enumerate(v) if val==min_val and a[ind] is True]
                 min_index=0
                 # 작은 값이 여러개면 인덱스 값이 낮은 값을 비활성화
                 if len(min_val_index)>1:
                     min_index=min(min_val_index)
                 else :
-                    min_val_index[0]
+                    min_index=min_val_index[0]
                 a[min_index]=False
                 # 위치 지우기
-                if len(marbles[(r[min_index],c[min_index])])>1:
-                    marbles[(r[min_index],c[min_index])].remove(min_index)
-                else: del marbles[(r[min_index],c[min_index])]
-        
-    # print(marbles)
+                marbles[(r[min_index],c[min_index])].remove(min_index)
+
 
 move_marbles()
 result=0
