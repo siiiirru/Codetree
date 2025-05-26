2f6e2e0bd81ea8370e0ae7415242ea1a01dc0c44
Siyun Park <eqeee@naver.com>
2025-05-26 12:02:20 +0900

    Title: 서로 다른 BST 개수 세기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250526/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 250526/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -13,18 +13,28 @@ dp=[1]*(n+1)
 # dp[4]=dp[3]*2+(dp[1]*dp[2])*2
 # dp[5]=dp[4]*2+(dp[1]*dp[3])*2+(dp[1]*dp[2])*2
 # dp[6]=dp[5]*2+(dp[1]*dp[4])*2+(dp[2]*dp[3])*2
-# dp[i]=(dp[0]*dp[i-1])*2+(dp[1]*dp[i-2])*2+(dp[1]*dp[i-3])*2
+
+# 점화식
+# dp[i]=(dp[0]*dp[i-1])*2+(dp[1]*dp[i-2])*2+(dp[2]*dp[i-3])*2+...
+# 만약 i가 홀수면 dp[j]와 dp[i-(j+1)]이 같아질때 + dp[j]*dp[j]해줌
 
 for i in range (1, n+1):
-    if i<=2:
-        dp[i]=i
-    elif i==3:
-        dp[i]=5
-    else :
-        sum=0
-        for j in range(0,(i//2)+(i%2)):
-            if j==i-(j+1) : sum+=(dp[j]*dp[j])
-            else : sum+=(dp[j]*dp[i-(j+1)])*2
-        dp[i]=sum
+    sum=0
+    for j in range(0,(i//2)+(i%2)):
+        if j==i-(j+1) : sum+=(dp[j]*dp[j])
+        else : sum+=(dp[j]*dp[i-(j+1)])*2
+    dp[i]=sum
 
 print (dp[n])
+
+
+#카탈린 수 점화식은 이건데 대칭되는 부분 중복계산이 들어가게 됨
+#dp[0]=1
+#dp[1]=dp[0]*dp[0]
+#dp[2]=dp[0]*dp[1]+dp[1]*dp[0]
+
+# for i in range(1,n+1):
+#     sum=0
+#     for j in range (i):
+#         sum+=dp[j]*dp[(i-1)-j]
+#     dp[i]=sum
