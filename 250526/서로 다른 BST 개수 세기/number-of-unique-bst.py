992f30755c9cf0fc2c3824eb8159941f50b095a0
Siyun Park <eqeee@naver.com>
2025-05-26 11:15:43 +0900

    Title: 서로 다른 BST 개수 세기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250526/서로 다른 BST 개수 세기/number-of-unique-bst.py
+++ 250526/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -5,13 +5,14 @@ dp=[1]*(n+1)
 
 #즉, 루트를 k로 고정하면,
 #왼쪽 서브트리로 만들 수 있는 BST의 개수 × 오른쪽 서브트리로 만들 수 있는 BST의 개수가
-#루트를 k로 할 때 만들 수 있는 BST 개수가 되는 거야.
-#DP[0] = 1 (노드가 없을 때도 하나의 BST로 본다)
-#DP[1] = 1
+#루트를 k로 할 때 만들 수 있는 BST 개수가 되는 것
+# dp[0] = 1 (노드가 없을 때도 하나의 BST로 본다)
+# dp[1] = 1
 # dp[2]=dp[1]*2
 # dp[3]=dp[2]*2+dp[1]
-# dp[4]=dp[3]*2+(dp[1]*dp[3])+(dp[2]*dp[1])
-# dp[i]=(dp[i-1]*2)+dp[0]*dp[i-1]+dp[1]*dp[i-2]
+# dp[4]=dp[3]*2+(dp[0]*dp[2])*2
+# dp[5]=dp[4]*2+(dp[0]*dp[3])*2+(dp[1]*dp[2])*2
+# dp[i]=(dp[i-1]*2)+(dp[0]*dp[i-2])+(dp[1]*dp[i-3])*2
 
 for i in range (1, n+1):
     if i<=2:
@@ -19,9 +20,10 @@ for i in range (1, n+1):
     elif i==3:
         dp[i]=5
     else :
-        sum=dp[i-1]*2
-        for j in range(i//2):
-            sum+=dp[j]*dp[i-(j+1)]
+        sum=0
+        for j in range((i//2)+(i%2)):
+            if j==((i//2)-1):sum+=dp[i-1]*2
+            else: sum+=(dp[j]*dp[i-(j+2)])*2
         dp[i]=sum
 
 print (dp[n])
